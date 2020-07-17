9d67aa72537ea28b86c032b473baf7815b918fdc
Graham Christensen <graham@grahamc.com>
2020-07-17 12:29:09 -0400

    Playing with virtualbox

---
+++ network.nix
@@ -0,0 +1,23 @@
+{
+  network = {}; # .storage.legacy.databasefile = "./deployment.nixops";
+
+  example = { pkgs, lib, ... }: {
+    deployment.targetEnv = "virtualbox";
+    boot = {
+      loader.timeout = lib.mkForce 2;
+
+      systemdExecutable = toString (
+        pkgs.writeShellScript "systemd-shim" ''
+          echo "Here come some shenanigans."
+          set -eux
+          ${pkgs.iproute}/bin/ip netns add virtual
+          ${pkgs.coreutils}/bin/touch /var/run/netns/physical
+          ${pkgs.utillinux}/bin/mount -o bind /proc/self/ns/net /var/run/netns/physical
+          exec ${pkgs.iproute}/bin/ip netns exec ${if true then "physical" else "virtual"} systemd
+        ''
+      );
+    };
+
+    services.mingetty.autologinUser = "root";
+  };
+}
--- shell.nix
+++ shell.nix
@@ -1,8 +1,40 @@
 let
   pkgs = import <nixpkgs> {};
+  my-nixops-with-vbox = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./.;
+    overrides = pkgs.poetry2nix.overrides.withDefaults (
+      self: super: {
+        zipp = super.zipp.overridePythonAttrs (
+          old: {
+            propagatedBuildInputs = old.propagatedBuildInputs ++ [
+              self.toml
+            ];
+          }
+        );
+
+        nixops = super.nixops.overridePythonAttrs (
+          old: {
+            format = "pyproject";
+            buildInputs = old.buildInputs ++ [ self.poetry ];
+          }
+        );
+
+        nixopsvbox = super.nixopsvbox.overridePythonAttrs (
+          old: {
+            format = "pyproject";
+            buildInputs = old.buildInputs ++ [ self.poetry ];
+          }
+        );
+      }
+    );
+
+  };
 in
 pkgs.mkShell {
   buildInputs = [
-    pkgs.hello
+    my-nixops-with-vbox
   ];
+
+  NIXOPS_DEPLOYMENT = "nixops-with-vbox";
+  NIX_PATH = "nixpkgs=channel:nixos-unstable-small";
 }
